ca6fbb533e01d22239c0d203e1c891338a579a6f
Bram Gotink <bram@gotink.me>
2021-10-27 23:23:54 +0200

    {feature} support `data:` URLs

--- src/data.ts
+++ src/data.ts
@@ -49,14 +49,33 @@ export async function getSourceMap(
 
   const resolved = new URL(match[1]!, url);
 
-  if (resolved.protocol === 'file:') {
-    return JSON.parse(await fs.readFile(resolved, 'utf8'));
-  } else {
-    const response = await fetch(resolved.href, {
-      method: 'GET',
-    });
+  switch (resolved.protocol) {
+    case 'file:':
+      return JSON.parse(await fs.readFile(resolved, 'utf8'));
+    case 'data:': {
+      if (!/^application\/json[,;]/.test(resolved.pathname)) {
+        return undefined;
+      }
 
-    return await response.json();
+      const comma = resolved.pathname.indexOf(',');
+      const rawData = resolved.pathname.slice(comma + 1);
+      const between = resolved.pathname
+        .slice('application/json;'.length, comma)
+        .split(';');
+
+      const dataString = between.includes('base64')
+        ? Buffer.from(rawData, 'base64url').toString('utf8')
+        : rawData;
+
+      return JSON.parse(dataString);
+    }
+    default: {
+      const response = await fetch(resolved.href, {
+        method: 'GET',
+      });
+
+      return await response.json();
+    }
   }
 }
 
